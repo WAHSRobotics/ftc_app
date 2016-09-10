097f9a0aed864bdb58657711209d7e6fb864f84f
Nathaniel Glover <nathanielsglover@gmail.com>
2016-09-10 19:09:10 -0400

    Update project to FTC SDK 2.2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Test TeleOp", group = "HME OpModes")
+public class Test extends OpMode {
+    @Override
+    public void init() {
+        
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
