57de087febe2b27ecd94f1ffa4312c185758a3a3
Nathaniel Glover <nathanielsglover@gmail.com>
2016-09-29 21:26:26 -0400

    Remove old OpMode, create simple testing environment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OutputTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OutputTesting.java
@@ -1,27 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
 public class OutputTesting {
-    public static final double POWER_MIN = 0.05;
-    public static final double POWER_MAX = 1.0;
-    public static final double POWER_INTERVAL = 0.05;
-
     public static void main(String[] args) {
-        for(double i = 0.0; i < 1.1; i += 0.01) {
-            System.out.println(scalePower(i));
-        }
-    }
+//        PowerScale powerScale = new PowerScale(0.05, 1.0);
+//        double lastVal = 0.0, currVal;
+//
+//        for(double i = -1.1; i < 1.1; i += 0.01) {
+//            currVal = powerScale.scalePower(i);
+//            System.out.println(currVal + " --- diff == " + MathUtil.setSignificantFigures(currVal - lastVal, 2));
+//            lastVal = currVal;
+//        }
 
-    public static double scalePower(double power) {
-        double scaledPower;
-
-        if(POWER_MIN <= power && power <= POWER_MAX) {
-            scaledPower = ((int) (power / POWER_INTERVAL)) * POWER_INTERVAL;
-
-            scaledPower = Math.round(scaledPower * 10000.0) / 10000.0;;
-        } else {
-            scaledPower = 0.0;
-        }
-
-        return scaledPower;
+        System.out.println(Math.tan(45));
     }
 }
