d85c4e7af63cc4d7dfe4fd4e5fbd367acb361abc
Sage Wibberley <sbww0@schls.albco>
2016-11-03 17:44:02 -0400

    Modify servo test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/ServoTest.java
@@ -4,22 +4,35 @@ package org.firstinspires.ftc.teamcode.opmode.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Servo Testing", group = "Tests")
-public class ServoTest extends LinearOpMode {
+public class ServoTest extends OpMode {
+    CRServo left, right;
+
     @Override
-    public void runOpMode() throws InterruptedException {
-        Servo servo1 = hardwareMap.servo.get("s1");
-        Servo servo2 = hardwareMap.servo.get("s2");
-        Servo servo3 = hardwareMap.servo.get("s3");
-        Servo servo4 = hardwareMap.servo.get("s4");
+    public void init() {
+        left = hardwareMap.crservo.get("s_left");
+        right = hardwareMap.crservo.get("s_right");
 
-        waitForStart();
+        left.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
 
-        servo1.setPosition(1.0);
-        servo2.setPosition(1.0);
-        servo3.setPosition(1.0);
-        servo4.setPosition(1.0);
+    @Override
+    public void loop() {
+        if(gamepad1.left_trigger > 0.1) {
+            left.setPower(-gamepad1.left_trigger);
+            right.setPower(-gamepad1.left_trigger);
+        }
+        else if(gamepad1.right_trigger > 0.1) {
+            left.setPower(gamepad1.right_trigger);
+            right.setPower(gamepad1.right_trigger);
+        }
+        else {
+            left.setPower(0);
+            right.setPower(0);
+        }
     }
 }
