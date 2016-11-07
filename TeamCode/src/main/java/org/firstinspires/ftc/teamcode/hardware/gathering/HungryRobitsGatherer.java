2894344d3435a4ac368bbe49d18246cb71abb9d9
Sage Wibberley <sagewibberley@gmail.com>
2016-11-07 16:03:52 -0500

    Implement HungryRobitsGatherer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/gathering/HungryRobitsGatherer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/gathering/HungryRobitsGatherer.java
@@ -1,15 +1,28 @@
 package org.firstinspires.ftc.teamcode.hardware.gathering;
 
 
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class HungryRobitsGatherer extends Gatherer {
+    private CRServo left, right;
+    private Servo armLeft, armRight;
+
     @Override
     public void gatherControlled(Gamepad gamepad) {
-
+        if(gamepad.right_trigger >= 0.2){
+            left.setPower(gamepad.right_trigger);
+            right.setPower(gamepad.right_trigger);
+        }
+        if(gamepad.left_trigger >= 0.2) {
+            left.setPower(-gamepad.left_trigger);
+            right.setPower(-gamepad.left_trigger);
+        }
     }
 
     @Override
@@ -19,7 +32,13 @@ public class HungryRobitsGatherer extends Gatherer {
 
     @Override
     public void init(HardwareMap hardwareMap) {
+        left = hardwareMap.crservo.get("gather_left");
+        right = hardwareMap.crservo.get("gather_right");
+        armLeft = hardwareMap.servo.get("gather_arm_left");
+        armRight = hardwareMap.servo.get("gather_arm_right");
 
+        left.setDirection(DcMotorSimple.Direction.REVERSE);
+        armLeft.setDirection(Servo.Direction.REVERSE);
     }
 
     @Override
