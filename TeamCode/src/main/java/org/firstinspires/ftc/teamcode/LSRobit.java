0aa334d391735d336626facb3f6948d14b0e7080
theclassymuffin <shaunaksarker506@gmail.com>
2016-11-12 01:15:09 -0500

    possible error will work on during meeting

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LSRobit.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by Shaun Sarker on 11/12/2016.
+ */
+
+    import com.qualcomm.robotcore.hardware.DcMotor;
+    import com.qualcomm.robotcore.hardware.DcMotorSimple;
+    import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+    public class LSRobit {
+        public DcMotor leftfront;
+        public DcMotor rightfront;
+        public DcMotor leftback;
+        public DcMotor rightback;
+
+
+
+        private HardwareMap hwMap;
+
+        public void init(HardwareMap ahwMap){
+            hwMap = ahwMap;
+
+            leftfront = hwMap.dcMotor.get("lf");
+            rightfront = hwMap.dcMotor.get("rf");
+            leftback = hwMap.dcMotor.get("lb");
+            rightback = hwMap.dcMotor.get("rb");
+            leftfront.setDirection(DcMotor.Direction.FORWARD);
+            rightfront.setDirection(DcMotorSimple.Direction.REVERSE);
+            leftback.setDirection(DcMotorSimple.Direction.FORWARD);
+            rightback.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+            leftfront.setPower(0);
+            rightfront.setPower(0);
+            leftback.setPower(0);
+            rightback.setPower(0);
+
+            leftfront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            rightfront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            leftback.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            rightback.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }}
