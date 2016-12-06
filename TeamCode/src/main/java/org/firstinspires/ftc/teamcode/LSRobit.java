6d94065d3d38d9fc6a330dcbcfd68dba60f18945
Jeffrey Lavezzo <jeff@lavezzo.com>
2016-12-05 20:28:11 -0500

    moved power scale method from last year into this year's code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LSRobit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LSRobit.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
 /** Created By The Very Dank Meme Lord Shaun "Dank Indian" Sarker
  * I like Memes
  * Do You Like Memes?
@@ -32,5 +34,33 @@ public class LSRobit {
         rightfront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         leftback.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightback.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    private float scalePower(float power) {
+        float scaledPower = 0.0f;
+
+        power = Range.clip(power, -1, 1);
+        float[] possiblePowerValues = {
+                0.00f, 0.05f, 0.09f, 0.10f, 0.12f,
+                0.15f, 0.18f, 0.24f, 0.30f, 0.36f,
+                0.43f, 0.50f, 0.60f, 0.72f, 0.85f,
+                1.00f, 1.00f
+        };
+
+        int powerIndex = (int)(power * 16.0);
+
+        if (powerIndex < 0) {
+            powerIndex = -powerIndex;
+        } else if (powerIndex > 16) {
+            powerIndex = 16;
+        }
+
+        if (power < 0) {
+            scaledPower = -possiblePowerValues[powerIndex];
+        } else {
+            scaledPower = possiblePowerValues[powerIndex];
+        }
+
+        return scaledPower;
     }
 }
