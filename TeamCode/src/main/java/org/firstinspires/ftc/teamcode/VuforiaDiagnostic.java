47bc019a7baa008d09699720c00995c3a02d0280
Srmh9 <srmh9@k12albemarle.org>
2016-12-13 17:11:12 -0500

    Vuforia

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaDiagnostic.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by srmh9 on 11/29/2016.
+ */
+
+public class VuforiaDiagnostic extends LinearOpMode {
+    public VuforiaDiagnostic() {
+        super();
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+
+    @Override
+    public synchronized void waitForStart() throws InterruptedException {
+        super.waitForStart();
+    }
+}
