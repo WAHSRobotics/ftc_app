89e687770cf8989ae47cc1f51e275785bb842a64
Nathaniel Glover <nathanielsglover@gmail.com>
2017-02-26 00:06:22 -0500

    Robots are dumb

---
+++ Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/opmode/test/HardwareMapTest.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.team9202hme.opmode.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+public class HardwareMapTest extends LinearOpMode {
+}
---
+++ Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/program/autonomous/ShootAutonomousProgram.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.team9202hme.program.autonomous;
+
+import org.firstinspires.ftc.team9202hme.program.AutonomousProgram;
+
+
+public class ShootAutonomousProgram extends AutonomousProgram {
+}
